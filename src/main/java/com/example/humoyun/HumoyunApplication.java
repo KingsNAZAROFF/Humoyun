0ca5269058235c18019cad85bbedfaa80b592561
AbdukarimNazarov <nazarov-abdukarim@nail.ru>
2023-10-30 15:27:32 +0500

    first commit

---
+++ src/main/java/com/example/humoyun/HumoyunApplication.java
@@ -0,0 +1,13 @@
+package com.example.humoyun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HumoyunApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HumoyunApplication.class, args);
+    }
+
+}
